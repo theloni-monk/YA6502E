fd386cba9f6298312f2349cff108a76a050f1fb8
Theo <13920881+theloni-monk@users.noreply.github.com>
2020-04-18 22:31:36 -0700

    created implementation files

---
+++ EMU_6502/Operations.cpp
@@ -0,0 +1,27 @@
+﻿#include "Operations.h"
+
+//WRITEME all opcodes
+operation _ADC = [](CPU *c, op_code_params *o) -> void
+{
+	
+};
+
+
+operation Ops[] = {
+	_ADC,
+	_AND, _ASL,
+	_BCC, _BCS, _BEQ, _BIT, _BMI, _BNE, _BPL, _BRK, _BVC, _BVS,
+	_CLC, _CLD, _CLI, _CLV, _CMP, _CPX, _CPY,
+	_DEC, _DEX, _DEY,
+	_EOR,
+	_INC, _INX, _INY,
+	_JPM, _JSR,
+	_LDA, _LDX, _LDY, _LDR,
+	_NOP,
+	_ORA,
+	_PHA, _PHP, _PLA, _PLP,
+	_ROL, _ROR, _RTI, _RTS,
+	_SBC, _SEC, _SED, _SEI, _STA, _STX, _STY,
+	_TAX, _TAY, _TSX, _TXA, _TXS, _TYA
+};
+
---
+++ EMU_6502/main.cpp
@@ -0,0 +1,12 @@
+// EMU_6502.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n"; 
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu

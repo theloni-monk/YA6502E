dff97219edd46f27f7a793bb0357a4a63782364f
Theo <13920881+theloni-monk@users.noreply.github.com>
2020-05-24 02:15:18 -0700

    fixed linker bug

--- EMU_6502/MemoryInterface.hpp
+++ EMU_6502/MemoryInterface.hpp
@@ -6,9 +6,9 @@ class MemoryInterface
 public:
 	unsigned char* addressSpace;
 	
-	virtual unsigned char read(uint16_t address);
-	virtual void write(uint16_t address, char byte);
+	virtual unsigned char read(uint16_t address) { return addressSpace[address]; };
+	virtual void write(uint16_t address, char byte) { addressSpace[address] = byte; };
 
 	MemoryInterface(unsigned char* mem_ptr) { this->addressSpace = mem_ptr; };
-	~MemoryInterface() { delete addressSpace; };
+	//virtual ~MemoryInterface() { delete addressSpace; };
 };
